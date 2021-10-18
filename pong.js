a3a7b1441908da6beddba969b574c50a8af680f5
John Doe <janinelorenz@gmx.ch>
2021-10-18 13:46:57 +0200

    first try

--- pong.js
+++ pong.js
@@ -1,14 +1,19 @@
 var xBall = Math.floor(Math.random() * 300) + 50;
-var yBall = 50;
+var yBall = Math.floor(Math.random() * 300) + 50;
 var xSpeed = (2, 7);
 var ySpeed = (-7, -2);
 var score = 0
+//size ball
+var d = 50
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-}
 
+// ball moves random
+xBall = random(0, width);
+yBall = random(0, height);
+}
 function draw() {
   // Background
   background(2, 3, 5);
@@ -22,6 +27,7 @@ function draw() {
   // TO DO 1: Bringe den Balken dazu der Maus auf der x-Achse zu folgen. 
   // I Made the bar follow the mouse on the x-axis.
   let c = color(255, 204, 0);
+  
   fill(c);
   rect(mouseX, windowHeight-15, 120, 15);
 
@@ -33,14 +39,18 @@ function draw() {
 
   xBall += xSpeed;
   fill(73, 162, 178);
-  ellipse(windowHeight/2, xBall, 20, 20);
+ 
+  ellipse(xBall, yBall, d);
   // TO DO 2: Schaffst du es, dass sich der Ball frei bewegt?
   // Can you make the ball move freely?
 
-  if(xBall>windowWidth-10){
-    xSpeed *= -1;
-    if(xBall>windowWidth+10){
-      xSpeed *= +1;
+  xBall = xBall + xSpeed;
+  yBall = yBall + ySpeed;
+
+  if(xBall > windowWidth - d/2 || xBall < d/2 ){
+    xSpeed = -xSpeed;
+    if(yBall > windowWidth - d/2 || yBall < d/2 ){
+      ySpeed *= -ySpeed;
     }  
   } 
   // TO DO 3: Lass den Ball von den Seitenrändern abprallen (windowWidth/windowHeight)
