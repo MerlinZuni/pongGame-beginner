29fef213204b698369ab1d7aeac4d93a1c24cd10
Merlin Zuni <merlin.zuni@gmail.com>
2021-10-18 10:54:20 +0200

    Second Paddle added
    I have created a second paddle

--- pong.js
+++ pong.js
@@ -19,11 +19,17 @@ function draw() {
   textSize(48);
   text("SCORE: " + score, 20, 55);
 
+  // TO DO 1: Bringe den Balken dazu der Maus auf der x-Achse zu folgen. 
+  // I Made the bar follow the mouse on the x-axis.
   let c = color(255, 204, 0);
   fill(c);
-  rect(mouseX, windowHeight-10, 90, 35);
-  // TO DO 1: Bringe den Balken dazu der Maus auf der x-Achse zu folgen. 
-  // Make the bar follow the mouse on the x-axis. 
+  rect(mouseX, windowHeight-15, 120, 15);
+
+  // I've created a second paddle on the top
+  let b = color(0, 204, 255);
+  fill(b);
+  rect(0 , 0, 120, 15);
+
 
   xBall += xSpeed;
   fill(73, 162, 178);
@@ -41,5 +47,5 @@ function draw() {
   // Bounce the ball off the edges of the page (windowWidth/windowHeight)
 
   // TO DO 4: Lass den Ball vom Balken aprallen, falls sie sich berühren
-
+  // 1 if statements on our own how to calculate
 }
